b6607bfa9088576fdd9105568833dc2675342899
CostaLGC <lg2costa@gmail.com>
2024-05-30 18:39:26 -0300

    Script 1

--- script1.sh
+++ script1.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-# Script em Bash que imprime "Hello, world!"
-echo "Hello, world!"
+# # Script em Bash que imprime "Hello, world!"
+# echo "Hello, world!"
+ls
+cd /etc
+ls
+cd
+echo $SHELL
+
+which bash
+
+
