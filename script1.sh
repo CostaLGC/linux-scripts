59f9744d1f3f057eeb8f2b10b92b580b1fcdb3f3
CostaLGC <lg2costa@gmail.com>
2024-06-05 08:56:31 -0300

    Script 1

--- script1.sh
+++ script1.sh
@@ -3,9 +3,14 @@ ls -lah
 echo $SHELL
 which bash
 pwd
-
 echo "Hello World!"
 echo "My current working directory is:"
 pwd
 myname="Luiz"
 echo $myname
+myage="40"
+
+
+word="fun"
+files=$(ls)
+echo $files
