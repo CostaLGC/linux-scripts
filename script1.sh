dc43f6938a063eea40a5eeaf1248631b50f64cf2
CostaLGC <lg2costa@gmail.com>
2024-06-19 05:09:52 -0300

    Script 1

--- script1.sh
+++ script1.sh
@@ -101,7 +101,19 @@ then
     echo "$command is available, let's run it..."
 else
     echo "$command os NOT available, installing it..."
-    sudo apt-get update && sudo apt install -y htop
+    #sudo apt-get update && sudo apt install -y htop
+fi
+
+$command
+
+
+command=htop
+if command -v $command
+then
+    echo "$command is available, let's run it..."
+else
+    echo "$command is NOT available, installing it..."
+    #sudo apt update && sudo apt install -y $command
 fi
 
 $command
